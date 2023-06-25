f6a48ab70ce0164b3697acbe46925253ff5ffd58
MMcClure11 <mmcclure0100@gmail.com>
2023-06-24 20:30:49 -0600

    Add lookups and events table, import their data

--- scripts/import.sql
+++ scripts/import.sql
@@ -1,19 +1,14 @@
-drop table if exists master_plan;
-create table master_plan(
-  start_time_utc text,
-  duration text,
-  date text,
-  team text,
-  spass_type text,
-  target text,
-  request_name text,
-  library_definition text,
-  title text,
-  description text
-);
+drop schema if exists import cascade;
+create schema import;
 
-create schema if not exists import;
-drop table if exists import.master_plan;
+
+drop table if exists events cascade;
+drop table if exists teams cascade;
+drop table if exists targets cascade;
+drop table if exists spass_types cascade;
+drop table if exists requests cascade;
+drop table if exists event_types cascade;
+
 create table import.master_plan(
   start_time_utc text,
   duration text,
