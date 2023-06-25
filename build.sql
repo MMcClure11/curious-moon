72bfb5f61c5a82a46312add10ddeac617700e84f
MMcClure11 <mmcclure0100@gmail.com>
2023-06-24 20:02:19 -0600

    Add dropping and recreating import.master plan table
    Prior to this it was importing to the already existing import.master_plan table

--- build.sql
+++ build.sql
@@ -11,4 +11,19 @@ create table master_plan(
   title text,
   description text
 );
+
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
 COPY import.master_plan FROM '/Users/mmcclure/Code/curious_data/data/master_plan.csv' WITH DELIMITER ',' HEADER CSV;
--- scripts/import.sql
+++ scripts/import.sql
@@ -11,3 +11,18 @@ create table master_plan(
   title text,
   description text
 );
+
+create schema if not exists import;
+drop table if exists import.master_plan;
+create table import.master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
